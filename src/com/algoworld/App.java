adcfd37502581d0411af17f7a2ca8633015576bb
Swagatkumar <swagatkumar001@gmail.com>
2021-07-16 08:11:14 +0530

    jump search

--- src/com/algoworld/App.java
+++ src/com/algoworld/App.java
@@ -4,9 +4,9 @@ import com.algoworld.searchingsorting.Search;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] arr = {10,20,30,33,40,47,50,60,69};
-        int element = 69;
-        int position = Search.binaryr(arr, element);
+        int arr[] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610};
+        int element = 55;
+        int position = Search.jump(arr, element);
         System.out.println(position==-1?"Could not find "+element:"Element "+element+" found at position "+position);
     }
 }
--- src/com/algoworld/searchingsorting/Search.java
+++ src/com/algoworld/searchingsorting/Search.java
@@ -80,4 +80,27 @@ public class Search {
     public static int binaryr(int[] arr, int element){
         return binaryRecursive(arr, 0, arr.length-1, element);
     }
+
+    public static int jump(int[] arr, int element){
+        int n = arr.length;
+        int step = (int)Math.floor(Math.sqrt(n));
+        int prev = 0;
+        while (arr[Math.min(step, n)-1]<element) {
+            prev = step;
+            step += (int)Math.floor(Math.sqrt(n));
+            if(prev>=n)
+                return -1;
+        }
+
+        while (arr[prev]<element) {
+            prev++;
+            if(prev == Math.min(step, n))
+                return -1;
+        }
+
+        if(arr[prev]==element)
+            return prev;
+            
+        return -1;
+    }
 }
